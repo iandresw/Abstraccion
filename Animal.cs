50fbd00b0296a437e56468ffa24595b2187263c6
AndresB <rbejarano@unah.hn>
2024-04-24 19:08:05 -0600

    Agregar archivos de proyecto.

---
+++ Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraccion
+{
+    public abstract  class Animal
+    {
+        public abstract string Nombre { get; set; }    
+        public string Tamanio {  get; set; }    
+        public string Color { get; set;}
+        public string Familia { get; set;}
+
+        public abstract void Imprimir();
+    }
+}
---
+++ Vertebrado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraccion
+{
+    public class Vertebrado : Animal
+    {
+        public int id { get; set; }
+
+        public int vertebradoId { get; set; }
+
+        public string vertebradoName { get; set; }
+
+
+
+        public override string Nombre { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void Imprimir()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
